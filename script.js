eeb66ef02f03f98885acff0a527c0ef26524a27c
Jean Occean <joccean2018@bklawtech.com>
2017-08-07 19:16:16 +0000

    my comment

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 $(document).ready(function() {
 
-$('ul li').click(function(){
-
-    // … Code to change active tab goes here ...
-    
-    var clicked_href = $(this).children('a').attr('href');
-    $('img').hide();
-    $(clicked_href).show();
-    
-    
-    
-    
-});
-
  $('[data-toggle="popover"]').popover();
  
 });
