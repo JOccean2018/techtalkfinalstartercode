52500a632b71cd0706d0ab2ff4d856ae2173ed03
Jean Occean <joccean2018@bklawtech.com>
2017-08-10 21:07:35 +0000

    first commit

--- script.js
+++ script.js
@@ -2,4 +2,28 @@ $(document).ready(function() {
 
  $('[data-toggle="popover"]').popover();
  
+ 
+function myMap() {
+    var myCenter = new google.maps.LatLng(40.6797258,-73.9864152);
+var mapProp= {
+    center:new google.maps.LatLng(40.6797258,-73.9864152),
+    zoom:5,
+    
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+var marker = new google.maps.Marker({map:map,position: myCenter});
+
+
+
+
+
+
+}
+
+myMap();
+ 
+$('.carousel').carousel({
+  interval: 2000
+})
+ 
 });
