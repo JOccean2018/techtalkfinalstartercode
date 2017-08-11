82e45554d10eefd89fccf071395f20535c3561f2
Jean Occean <joccean2018@bklawtech.com>
2017-08-11 02:16:50 +0000

    night before presentation

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-$(document).ready(function() {
+$( document ).ready(function() {
 
- $('[data-toggle="popover"]').popover();
- 
- 
 function myMap() {
-    var myCenter = new google.maps.LatLng(40.6797258,-73.9864152);
-var mapProp= {
-    center:new google.maps.LatLng(40.6797258,-73.9864152),
-    zoom:5,
-    
-};
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-var marker = new google.maps.Marker({map:map,position: myCenter});
+    var myCenter = new google.maps.LatLng(40.6797258, -73.9864152);
+    var mapProp = {
+        center: new google.maps.LatLng(40.6797258, -73.9864152),
+        zoom: 5,
 
-
-
-
-
-
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var marker = new google.maps.Marker({ map: map, position: myCenter });
 }
 
 myMap();
- 
+
 $('.carousel').carousel({
-  interval: 2000
-})
- 
+    interval: 2000
+});
+
 });
